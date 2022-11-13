c07667f0831bc5489bc4367f0d22987064dd2de8
Ivan Maidana <ivan.maidana9912@gmail.com>
2022-11-12 21:31:30 -0300

    finish of the exercise of modules in python

---
+++ ejercicio 7 openbootcamp/main.py
@@ -0,0 +1,9 @@
+import operaciones as o
+def main():
+    print(f'La suma entre 4 y 6 es: {o.suma(4, 6)}')
+    print(f'La resta entre 4 y 6 es: {o.resta(4, 6)}')
+    print(f'La multiplicacion entre 4 y 6 es: {o.multiplicacion(4, 6)}')
+    print(f'La division entre 4 y 6 es: {o.division(4, 6)}')
+if __name__ == '__main__':
+    main()
+    
